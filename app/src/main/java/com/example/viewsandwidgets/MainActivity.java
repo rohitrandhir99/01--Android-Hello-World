d49aa366fde6f2504d4dc3118cf169a0d1813300
rohit <rohitran236@gmail.com>
2024-04-18 08:49:31 +0530

    first commit

--- app/src/main/java/com/example/viewsandwidgets/MainActivity.java
+++ app/src/main/java/com/example/viewsandwidgets/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.viewsandwidgets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,11 +18,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // creating a TextView variable
-        TextView myTitle;
-        myTitle = findViewById(R.id.title);
+        // user image
+        ImageView img;
+        img = findViewById(R.id.userImg);
+        img.setImageResource(R.drawable.userImg);
 
-        myTitle.setText("Hello From Java");
+        // title
+        TextView userTitle;
+        userTitle = findViewById(R.id.userTitle);
+        userTitle.setText("Jenny");
+
+        // edit text
+        EditText editText;
+        editText = findViewById(R.id.textInput);
+        editText.setText("Georgia");
+
+        // button
+        Button btn;
+        btn = findViewById(R.id.myBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public  void  onClick(View v) {
+                // this code will be executed when the button is clicked
+
+                // create a toast
+                Toast.makeText(MainActivity.this, "Account Created Successfully!!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
